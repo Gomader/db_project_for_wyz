074545413a62457337e063d5430dfc280424637a
Gomader <gh970818@hotmail.com>
2019-11-29 08:58:48 +0900

    01

--- index.php
+++ index.php
@@ -1,3 +1,20 @@
 <?php
+session_start();
 
 ?>
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>WYZ Cinema</title>
+    </head>
+    <body>
+        <div id="sign" style="height=100px;width=100px;margin-top:200px;margin-left:100px;">
+             <button onclick="signin()">sign in</button>
+             <button onclick="signup()">sign up</button>
+             <button onclick="signout()">sign out</button>
+        </div>
+        <script>
+            
+        </script>
+    </body>
+</html>
