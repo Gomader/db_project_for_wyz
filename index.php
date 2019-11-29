c1a8ae095d935b03aa3202141b8ed6cc32103eee
Gomader <gh970818@hotmail.com>
2019-11-29 09:34:56 +0900

    02

--- index.php
+++ index.php
@@ -14,14 +14,14 @@ session_start();
              <button onclick="signout()">sign out</button>
         </div>
         <div id="signform" style="width: 31.25rem;height: 18.75rem;margin:250px auto;border:0.0625rem solid #000000;">
-            <<form action="php/signin.php" id="inform" method="post">
+            <<form action="php/signin.php" id="inform" method="post" style="display:none;">
                 <label>ID:       </label><input type="text " name="username"><br>
-                <label>Password: </label><input type="password" name="password">
+                <label>Password: </label><input type="password" name="password"><br>
                 <input type="submit" value="Sign In"/>
             </form>
               
 
-            <form action="php/signup.php" method="post" id="upform">
+            <form action="php/signup.php" method="post" id="upform" >
                 <label>ID: You don't need ID, if you want to play with us, we will give you an ID and you just need a password, and you can use ID and password sign in.</label><br>
                 <label>Password: </label><input type="password" name="pwd"/><br>
                 <input type="submit" value="Sign Up"/>
@@ -29,7 +29,10 @@ session_start();
         </div>
         <script>
             function signin(){
-
+                document.getElementById("inform").style.display="block";
+            }
+            function signup(){
+                document.getElementById("upform").style.display="block";
             }
         </script>
     </body>
