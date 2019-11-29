eae3fe0a5a9779fbfe0204458d97cd35f8cfb996
Gomader <gh970818@hotmail.com>
2019-11-29 09:44:42 +0900

    02

--- index.php
+++ index.php
@@ -14,14 +14,14 @@ session_start();
              <button onclick="signout()">sign out</button>
         </div>
         <div id="signform" style="width: 31.25rem;height: 18.75rem;margin:250px auto;border:0.0625rem solid #000000;">
-            <<form action="php/signin.php" id="inform" method="post" style="display:none;">
+            <form action="php/signin.php" id="inform" method="post" style="display:none;">
                 <label>ID:       </label><input type="text " name="username"><br>
                 <label>Password: </label><input type="password" name="password"><br>
                 <input type="submit" value="Sign In"/>
             </form>
               
 
-            <form action="php/signup.php" method="post" id="upform" >
+            <form action="php/signup.php" method="post" id="upform" style="display:none;">
                 <label>ID: You don't need ID, if you want to play with us, we will give you an ID and you just need a password, and you can use ID and password sign in.</label><br>
                 <label>Password: </label><input type="password" name="pwd"/><br>
                 <input type="submit" value="Sign Up"/>
---
+++ php/siginup.php
@@ -0,0 +1,18 @@
+<?php
+require_once "mysql_entities_fix_string.php";
+require_once "../login/usr.php";
+$conn = new mysqli($hn,$un,$pw,$db);
+if ($conn->connect_error) die($conn->connect_error);
+
+$pwd = mysql_entities_fix_string($conn,$_POST["password"]);
+
+if(empty($pwd)) die("Sign up failed! You must use password");
+
+$query = "INSERT INTO usr VALUES(user_id,'$pwd')";
+if($conn->query($query) === true){
+    echo "<h1>Welcome to My Threat~</h1><a style='yellow'>Let's watch movie together!</a>";
+}else {
+    echo "Sign up Failed! Something wrong! Please try again!";
+}
+$conn->close();
+?>
